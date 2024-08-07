e0fc85926c9b8b361112b0acec2c10adf18d002f
Yann Kwadjio <kwadjionry@gmail.com>
2024-08-07 19:34:40 +0100

    first-commit

---
+++ src/main/java/com/myfacture/MyfactureApplication.java
@@ -0,0 +1,13 @@
+package com.myfacture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfactureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfactureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myfacture/MyfactureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfacture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfactureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
